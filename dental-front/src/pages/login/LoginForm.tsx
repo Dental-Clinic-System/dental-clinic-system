557083e1c4fcf01ba9ea3776b02fe97714c5869a
Sonor <sonor@Sonors-MacBook-Pro.local>
2022-03-09 16:59:44 +0800

    removed formik from login page

--- dental-front/src/pages/login/LoginForm.tsx
+++ dental-front/src/pages/login/LoginForm.tsx
@@ -1,7 +1,6 @@
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import { useFormik, Form, FormikProvider } from 'formik';
 import * as Yup from 'yup';
 
 import {
@@ -9,9 +8,9 @@ import {
     Stack,
     Checkbox,
     TextField,
-    FormControlLabel
+    FormControlLabel,
+    Button
 } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
 
 function LoginForm() {
     const navigate = useNavigate();
@@ -19,96 +18,93 @@ function LoginForm() {
     const [email, setEmail] = useState('')
     const [pass, setPass] = useState('')
     const [remember, setRemember] = useState(true)
-    const [error, setError] = useState({email: '', pass: ''})
+    const [errorEmail, setErrorEmail] = useState('')
+    const [errorPass, setErrorPass] = useState('')
 
     const LoginSchema = Yup.object().shape({
         email: Yup.string().email('Email must be a valid email address').required('Email is required'),
         password: Yup.string().required('Password is required')
     });
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            remember: true
-        },
-        validationSchema: LoginSchema,
-        onSubmit: () => {
-            navigate('/dashboard', { replace: true });
-        }
-    });
-
-    const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
-
     const handleShowPassword = () => {
         setShowPassword((show) => !show);
     };
 
-    const login = () => {
-        if (!email) {
-            alert('fuckass')
-            setError({email: 'Email is required', pass: error.pass})
-            setEmail('')
+    const login = async () => {
+        let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if (pass && email && email.match(validRegex)) {
+            navigate('/')
+        } else {
+            if (!email.match(validRegex)) {
+                setErrorEmail('Email is invalid')
+            }
+            if (!email) {
+                setErrorEmail('Email is required')
+            }
+            if (!pass) {
+                setErrorPass('Password is required')
+            }
         }
-        if (!pass) {
-            setError({pass: 'Password is required', email: error.email})
-            setPass('')
-        }
-        console.log(error);
     }
 
+    useEffect(() => {
+        if (email) {
+            setErrorEmail('')
+        }
+        if (pass) {
+            setErrorPass('')
+        }
+    }, [email, pass])
+
     return (
-        <FormikProvider value={formik}>
-            <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-                <Stack spacing={3}>
-                    <TextField
-                        fullWidth
-                        autoComplete="username"
-                        type="email"
-                        label="Email address"
-                        error={Boolean(email && error.email)}
-                        helperText={error.email}
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    
-
-                    <TextField
-                        fullWidth
-                        autoComplete="current-password"
-                        type={showPassword ? 'text' : 'password'}
-                        label="Password"
-                        {...getFieldProps('password')}
-                        error={Boolean(pass && error.pass)}
-                        helperText={error.pass}
-                        value={pass}
-                        onChange={(e) => setPass(e.target.value)}
-                    />
-                </Stack>
-
-                <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-                    <FormControlLabel
-                        control={<Checkbox onChange={() => setRemember(!remember)} checked={remember}  />}
-                        label="Remember me"
-                    />
-
-                    <Link component={RouterLink} variant="subtitle2" to="#" underline="hover">
-                        Forgot password?
-                    </Link>
-                </Stack>
-
-                <LoadingButton
+        <>
+            <Stack spacing={3}>
+                <TextField
                     fullWidth
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                    loading={isSubmitting}
-                    onClick={() => login()}
-                >
-                    Login
-                </LoadingButton>
-            </Form>
-        </FormikProvider>
+                    autoComplete="username"
+                    type="email"
+                    label="Email address"
+                    error={Boolean(errorEmail)}
+                    helperText={errorEmail}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+
+                <TextField
+                    fullWidth
+                    autoComplete="current-password"
+                    type={showPassword ? 'text' : 'password'}
+                    label="Password"
+                    error={Boolean(errorPass)}
+                    helperText={errorPass}
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                />
+            </Stack>
+
+            <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+                <FormControlLabel
+                    control={<Checkbox onChange={() => setRemember(!remember)} checked={remember} />}
+                    label="Remember me"
+                />
+
+                <Link component={RouterLink} variant="subtitle2" to="#" underline="hover">
+                    Forgot password?
+                </Link>
+            </Stack>
+
+            <Button
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+                onClick={() => login()}
+            >
+                Login
+            </Button>
+        </>
     );
 }
 
