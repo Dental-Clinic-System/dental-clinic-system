8ed27ec25133f5ca77467974fe0b8695cf2932bf
Zolboo Oktybri <zolboooktybri@Zolboos-MacBook-Pro.local>
2022-03-11 19:20:33 +0800

    [All] Added eslint configuration to all folders

--- dental-front/src/theme/ThemeProvider.tsx
+++ dental-front/src/theme/ThemeProvider.tsx
@@ -4,22 +4,22 @@ import { themeCreator } from './base';
 import { StylesProvider } from '@mui/styles';
 
 export const ThemeContext = React.createContext(
-  (themeName: string): void => { }
+  (themeName: string): void => {}
 );
 
 const ThemeProviderWrapper: React.FC = (props) => {
   const curThemeName = localStorage.getItem('appTheme') || 'PureLightTheme';
-  const [themeName, _setThemeName] = useState(curThemeName);
+  const [themeName, setThemeName] = useState(curThemeName);
   const theme = themeCreator(themeName);
-  const setThemeName = (themeName: string): void => {
+  const changeThemeName = (themeName: string): void => {
     localStorage.setItem('appTheme', themeName);
-    _setThemeName(themeName);
+    setThemeName(themeName);
   };
 
   return (
     <StylesProvider injectFirst>
-      <ThemeContext.Provider value={setThemeName}>
-        <ThemeProvider theme={theme}>{props.children}</ThemeProvider >
+      <ThemeContext.Provider value={changeThemeName}>
+        <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
       </ThemeContext.Provider>
     </StylesProvider>
   );
