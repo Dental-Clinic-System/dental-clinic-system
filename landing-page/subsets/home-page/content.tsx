b2bc730fda63c9ae06346be0f8d0dd3ba9124752
khash531 <n.ligfe@gmail.com>
2022-03-13 18:41:15 +0800

    implemented first version of new design

--- landing-page/subsets/home-page/content.tsx
+++ landing-page/subsets/home-page/content.tsx
@@ -6,60 +6,79 @@ import {
   TextField,
   Container,
   Typography,
+  InputLabel,
+  FormControl,
+  Select,
+  Box
 } from "@mui/material";
 import { EnvelopeIcon } from "../../components/";
 
 export const HomePageContent = () => {
   return (
     <Container>
-      <Grid container spacing={2} height="100vh" alignItems="center">
-        <Grid item lg={6} sm={12}>
-          <Typography variant="h5">deedlih</Typography>
-          <Typography variant="h3" fontWeight="bold">
-            dund
-          </Typography>
-          <Typography variant="h6">doodlih</Typography>
-        </Grid>
+      <Box
+        display="flex"
+        justifyContent="center">
+        <Box width="50vw">
+          <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid item xs={12} >
+              <InputLabel>
+                Эмнэлгийн нэр
+              </InputLabel>
+              <TextField fullWidth></TextField>
+              <Grid item xs={6}>
+                <InputLabel>
+                  Үйл ажиллагааны чиглэл
+                </InputLabel>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={6}>
+                <InputLabel>
+                Үйл ажиллагаа явуулж эхэлсэн огноо
+                </InputLabel>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel>
+                Холбоо барих утас
+                </InputLabel>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel>
+                Имэйл хаяг
+                </InputLabel>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel>
+                Эмнэлгийн вэб сайт
+                </InputLabel>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel>
+                Холбоо барих утас
+                </InputLabel>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel>
+                Имэйл хаяг
+                </InputLabel>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel>
+                Эмнэлгийн вэб сайт
+                </InputLabel>
+                <TextField fullWidth></TextField>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
 
-        <Grid item lg={6} sm={12}>
-          <Stack
-            spacing={3}
-            padding={4}
-            bgcolor="white"
-            borderRadius={3}
-            direction="column"
-          >
-            <Typography variant="h4" fontWeight="bold">
-              Send message
-            </Typography>
-            <Stack spacing={1}>
-              <TextField
-                id="outlined-basic"
-                label="Full Name"
-                variant="standard"
-              />
-              <TextField
-                id="outlined-basic"
-                label="E-mail"
-                variant="standard"
-              />
-              <TextField
-                id="outlined-basic"
-                label="Subject"
-                variant="standard"
-              />
-              <TextField
-                id="outlined-basic"
-                label="Message"
-                variant="standard"
-              />
-            </Stack>
-            <Button variant="contained" endIcon={<EnvelopeIcon />}>
-              Send
-            </Button>
-          </Stack>
-        </Grid>
-      </Grid>
     </Container>
   );
 };
