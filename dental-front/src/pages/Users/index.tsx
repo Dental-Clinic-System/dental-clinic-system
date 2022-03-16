e5a97feccc3fb6187b387ced8faf4602c2b6bfe5
Sonor <sonor@Sonors-MacBook-Pro.local>
2022-03-16 16:30:13 +0800

    Fixing Table Data Grid

--- dental-front/src/pages/Users/index.tsx
+++ dental-front/src/pages/Users/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as data from "./User.json"
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 
 const column = Object.keys(data.data[0]);
 const ThData = () => {
@@ -26,17 +26,19 @@ const Users = () => {
   console.log('data: ', data)
   return (
     <div>
-      {/* @ts-ignore */}
-      {data?.data.map((e, i) => {
-        return (
-          <div style={{ height: '100vh', width: '100vw' }}>
-            {/* <DataGrid rows={data.data} columns={data.data}/> */}
-              {ThData()}
-              {tdData()}
-          </div>
-        )
-      })}
+      {/* <DataGrid {...data} components={{Toolbar: GridToolbar}}/> */}
     </div>
   )
 }
 export default Users
+
+
+// {data?.data.map((e, i) => {
+//   return (
+//     <div style={{ height: '100vh', width: '100vw' }}>
+//       {/* <DataGrid rows={data.data} columns={data.data}/> */}
+//         {ThData()}
+//         {tdData()}
+//     </div>
+//   )
+// })}
