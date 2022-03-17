3f81dcf221c7f7dfc30e01154075e53859a69814
Zolboo Oktybri <zolboooktybri@Zolboos-MacBook-Pro.local>
2022-03-17 23:31:22 +0800

    Improved code quality

--- landing-page/subsets/home-page/content.tsx
+++ landing-page/subsets/home-page/content.tsx
@@ -1,45 +1,132 @@
-import React from "react";
-import {
-  Box,
-  Grid,
-  Stack,
-  Button,
-  MenuItem,
-  TextField,
-  Typography,
-  InputLabel,
-} from "@mui/material";
-import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
-import { useState } from "react";
+import React from 'react';
+import { Box, Grid, Button } from '@mui/material';
+import { useState } from 'react';
+import { QuestionSection } from './question-section';
+
+const data = {
+  services: [
+    { value: 'dental', label: 'Dental' },
+    { value: 'optic', label: 'Optic' },
+  ],
+  location: {
+    city: [
+      { value: 'ulaanbaatar', label: 'Ulaanbaatar' },
+      { value: 'darkhan', label: 'Darkhan' },
+    ],
+    district: [
+      { value: 'chingeltei', label: 'Чингэлтэй' },
+      { value: 'sukhbaatar', label: 'Сүхбаатар' },
+    ],
+    subdistrict: [
+      { value: 'fisrt', label: 'Нэгдүгээр' },
+      { value: 'second', label: 'Хоёрдугаар' },
+    ],
+  },
+};
 
 export const HomePageContent = () => {
-  const data = {
-    services: [{ value: 'dental', label: 'Dental', }, { value: 'optic', label: 'Optic', }],
-    location: {
-      city: [{ value: 'ulaanbaatar', label: 'Ulaanbaatar', }, { value: 'darkhan', label: 'Darkhan', },],
-      district: [{ value: 'chingeltei', label: 'Чингэлтэй', }, { value: 'sukhbaatar', label: 'Сүхбаатар', },],
-      subdistrict: [{ value: 'fisrt', label: 'Нэгдүгээр', }, { value: 'second', label: 'Хоёрдугаар', },],
-    },
+  const [formData, setFormData] = useState<any>({});
+
+  const onChange = (question: string, change: string) => {
+    setFormData({ ...formData, [question]: change });
   };
 
-  const [service, setService] = useState('Dental');
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setService(event.target.value);
+  const onSubmit = () => {
+    console.log(formData);
   };
 
   return (
-    <Box width="100%" display="flex" flexDirection="column" alignItems="center" marginY={5}>
-      <Box width="40%" >
-        <Typography variant="h5" gutterBottom fontWeight={600}>Эмнэлгийн мэдээлэл</Typography>
-        <Grid container>
-          <Grid item xs={12} >
+    <Box width="100vw" display="flex" flexDirection="column" alignItems="center" marginY={5}>
+      <Grid container justifyContent="center" columns={{ xs: 11 }} rowSpacing={5}>
+        <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+          <QuestionSection
+            onChange={onChange}
+            label="Эмнэлгийн мэдээлэл"
+            questions={[
+              { question: 'Эмнэлгийн нэр', type: 'input', grid: { xs: 12 }, error: !formData['Эмнэлгийн нэр'] },
+              {
+                question: 'Үйл ажиллагааны чиглэл',
+                type: 'selector',
+                grid: { xs: 5.4 },
+                error: !formData['Үйл ажиллагааны чиглэл'],
+                selections: ['Шүдний эмнэлэг'],
+              },
+              {
+                question: 'Үйл ажиллагаа явуулж эхэлсэн огноо',
+                type: 'input',
+                grid: { xs: 5.4 },
+                error: !formData['Үйл ажиллагаа явуулж эхэлсэн огноо'],
+              },
+              { question: 'Холбоо барих утас', type: 'input', grid: { xs: 12 }, error: !formData['Холбоо барих утас'] },
+              { question: 'Имэйл хаяг', type: 'input', grid: { xs: 12 }, error: !formData['Имэйл хаяг'] },
+              {
+                question: 'Эмнэлгийн вэб сайт',
+                type: 'input',
+                grid: { xs: 12 },
+                error: !formData['Эмнэлгийн вэб сайт'],
+              },
+            ]}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <QuestionSection
+            onChange={onChange}
+            label="Албан ёсны хаяг"
+            questions={[
+              { question: 'Аймаг / хот', type: 'input', grid: { xs: 5.4 }, error: !formData['Аймаг / хот'] },
+              { question: 'Дүүрэг', type: 'input', grid: { xs: 5.4 }, error: !formData['Дүүрэг'] },
+              { question: 'Хороо', type: 'input', grid: { xs: 5.4 }, error: !formData['Хороо'] },
+              { question: 'Дэлгэрэнгүй хаяг', type: 'input', grid: { xs: 12 }, error: !formData['Дэлгэрэнгүй хаяг'] },
+            ]}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <QuestionSection
+            label="Хариуцсан ажилтны мэдээлэл"
+            onChange={onChange}
+            questions={[
+              { question: 'Утас', type: 'number', grid: { xs: 12 }, error: !formData['Утас'] },
+              { question: 'Имэйл', type: 'input', grid: { xs: 12 }, error: !formData['Имэйл'] },
+              { question: 'Нэр', type: 'input', grid: { xs: 12 }, error: !formData['Нэр'] },
+              { question: 'Албан тушаал', type: 'input', grid: { xs: 12 }, error: !formData['Албан тушаал'] },
+            ]}
+          />
+        </Grid>
+
+        <Grid item container xs={6} justifyContent="flex-end">
+          <Grid item xs={4}>
+            <Button onClick={onSubmit} fullWidth variant="contained">
+              Илгээх
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+{
+  /* 
+  <Box>
+        <Typography variant="h5" gutterBottom fontWeight={600}>
+          Эмнэлгийн мэдээлэл
+        </Typography>
+        <Grid container width="40vw">
+          <Grid item xs={12}>
             <InputLabel>Эмнэлгийн нэр</InputLabel>
             <TextField fullWidth size="small"></TextField>
             <Stack direction="row" justifyContent="space-between">
               <Grid item xs={5.8}>
                 <InputLabel>Үйл ажиллагааны чиглэл</InputLabel>
-                <TextField fullWidth select value={service} onChange={handleChange} size="small">
+                <TextField
+                  fullWidth
+                  select
+                  value={service}
+                  onChange={handleChange}
+                  size="small"
+                >
                   {data.services.map((option) => (
                     <MenuItem key={option.value} value={option.value}>
                       {option.label}
@@ -49,98 +136,114 @@ export const HomePageContent = () => {
               </Grid>
               <Grid item xs={5.8}>
                 <InputLabel>Үйл ажиллагаа явуулж эхэлсэн огноо</InputLabel>
-                {/* <DesktopDatePicker
-                      label={"Date desktop"}
-                      inputFormat={"MM/dd/yyyy"}
-                      value={value}
-                      onChange={(newValue: Date | null) => {
-                        setValue(newValue);
-                      }}
-                      renderInput={(params) => <TextField {...params} />}
-                    /> */}
+                 <DesktopDatePicker
+                  label={"Date desktop"}
+                  inputFormat={"MM/dd/yyyy"}
+                  value={value}
+                  onChange={(newValue: Date | null) => {
+                    // setValue(newValue);
+                  }}
+                  renderInput={(params: any) => <TextField {...params} />}
+                /> 
+                </Grid>
+                </Stack>
+                <InputLabel>Холбоо барих утас</InputLabel>
+                <TextField fullWidth size="small"></TextField>
+                <InputLabel>Имэйл хаяг</InputLabel>
+                <TextField fullWidth size="small"></TextField>
+                <InputLabel>Эмнэлгийн вэб сайт</InputLabel>
+                <TextField fullWidth size="small"></TextField>
               </Grid>
-            </Stack>
-            <InputLabel>Холбоо барих утас</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-            <InputLabel>Имэйл хаяг</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-            <InputLabel>Эмнэлгийн вэб сайт</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-            <InputLabel>Холбоо барих утас</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-            <InputLabel>Имэйл хаяг</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-            <InputLabel>Эмнэлгийн вэб сайт</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-          </Grid>
-        </Grid>
-      </Box>
-
-      <Box width="40%" marginTop={5}>
-        <Typography variant="h5" gutterBottom fontWeight={600}>Албан ёсны хаяг</Typography>
-        <Grid container>
-          <Grid item xs={12} >
-            <Stack direction="row" justifyContent="space-between">
-              <Grid item xs={5.8}>
-                <InputLabel>Аймаг / хот</InputLabel>
-                <TextField fullWidth select value={service} onChange={handleChange} size="small">
-                  {data.location.city.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
+            </Grid>
+          </Box>
+    
+          <Box width="40%" marginTop={5}>
+            <Typography variant="h5" gutterBottom fontWeight={600}>
+              Албан ёсны хаяг
+            </Typography>
+            <Grid container>
+              <Grid item xs={12}>
+                <Stack direction="row" justifyContent="space-between">
+                  <Grid item xs={5.8}>
+                    <InputLabel>Аймаг / хот</InputLabel>
+                    <TextField
+                      fullWidth
+                      select
+                      value={service}
+                      onChange={handleChange}
+                      size="small"
+                    >
+                      {data.location.city.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                  </Grid>
+                  <Grid item xs={5.8}>
+                    <InputLabel>Дүүрэг</InputLabel>
+                    <TextField
+                      fullWidth
+                      select
+                      value={service}
+                      onChange={handleChange}
+                      size="small"
+                    >
+                      {data.location.district.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                  </Grid>
+                </Stack>
+                <Grid item xs={5.8}>
+                  <InputLabel>Хороо</InputLabel>
+                  <TextField
+                    fullWidth
+                    select
+                    value={service}
+                    onChange={handleChange}
+                    size="small"
+                  >
+                    {data.location.subdistrict.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Grid>
+                <Grid item xs={12}>
+                  <InputLabel>Дэлгэрэнгүй хаяг</InputLabel>
+                  <TextField fullWidth size="small"></TextField>
+                </Grid>
               </Grid>
-              <Grid item xs={5.8}>
-                <InputLabel>Дүүрэг</InputLabel>
-                <TextField fullWidth select value={service} onChange={handleChange} size="small">
-                  {data.location.district.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
+            </Grid>
+          </Box>
+    
+          <Box width="40%" marginTop={5}>
+            <Typography variant="h5" gutterBottom fontWeight={600}>
+              Хариуцсан ажилтны мэдээлэл
+            </Typography>
+            <Grid container>
+              <Grid item xs={12}>
+                <InputLabel>Утас</InputLabel>
+                <TextField fullWidth size="small"></TextField>
+                <InputLabel>Имэйл</InputLabel>
+                <TextField fullWidth size="small"></TextField>
+                <InputLabel>Нэр</InputLabel>
+                <TextField fullWidth size="small"></TextField>
+                <InputLabel>Албан тушаал</InputLabel>
+                <TextField fullWidth size="small"></TextField>
               </Grid>
-            </Stack>
-            <Grid item xs={5.8}>
-              <InputLabel>Хороо</InputLabel>
-              <TextField fullWidth select value={service} onChange={handleChange} size="small">
-                {data.location.subdistrict.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
+              <Grid marginTop={2} xs={12} display="flex" justifyContent="flex-end">
+                <Grid xs={5}>
+                  <Button fullWidth variant="contained">
+                    Илгээх
+                  </Button>
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <InputLabel>Дэлгэрэнгүй хаяг</InputLabel>
-              <TextField fullWidth size="small"></TextField>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Box>
-
-      <Box width="40%" marginTop={5} >
-        <Typography variant="h5" gutterBottom fontWeight={600}>Хариуцсан ажилтны мэдээлэл</Typography>
-        <Grid container>
-          <Grid item xs={12} >
-            <InputLabel>Утас</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-            <InputLabel>Имэйл</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-            <InputLabel>Нэр</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-            <InputLabel>Албан тушаал</InputLabel>
-            <TextField fullWidth size="small"></TextField>
-          </Grid>
-          <Grid marginTop={2} xs={12} display="flex" justifyContent="flex-end">
-            <Grid xs={5}>
-              <Button fullWidth variant="contained" >Илгээх</Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Box>
-
-    </Box>
-  );
-};
+          </Box>
+  */
+}
---
+++ landing-page/subsets/home-page/question-section.tsx
@@ -0,0 +1,145 @@
+import React, { useState } from 'react';
+import { Box, Grid, TextField, Typography, InputLabel, MenuItem } from '@mui/material';
+
+type grid = {
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+};
+type QuestionType = {
+  question: string;
+  type: 'input' | 'selector' | 'number';
+  grid: grid;
+  selections?: Array<string>;
+  error?: boolean;
+};
+type QuestionSectionType = {
+  label: string;
+  width?: string;
+  onChange?: (question: string, change: string) => void;
+  questions: Array<QuestionType>;
+};
+type QuestionInputType = {
+  onChange?: (question: string, change: string) => void;
+};
+
+export const QuestionSection = ({ label, width, questions, onChange }: QuestionSectionType) => {
+  return (
+    <Box width={width || '100%'}>
+      <Typography variant="h5" gutterBottom fontWeight={600}>
+        {label}
+      </Typography>
+
+      <Grid container justifyContent="space-between">
+        {questions.map((cur, index) => (
+          <Question onChange={onChange} key={`label-question-${index}`} {...cur} />
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+const Question = ({ question, grid, onChange, selections, error, type }: QuestionType & QuestionInputType) => {
+  const [value, setValue] = useState('');
+
+  if (type === 'selector')
+    return (
+      <Grid item {...grid}>
+        <InputLabel style={{ marginBottom: '10px', marginTop: '10px' }}>{question}</InputLabel>
+        <TextField
+          fullWidth
+          select
+          value={value}
+          onChange={(e) => {
+            onChange && onChange(question, e.target.value);
+            setValue(e.target.value);
+          }}
+          size="small"
+          error={error}
+        >
+          {selections?.map((option, index) => (
+            <MenuItem key={`${question}-option-${index}`} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Grid>
+    );
+
+  if (type === 'number')
+    return (
+      <Grid item {...grid}>
+        <InputLabel style={{ marginBottom: '10px', marginTop: '10px' }}>{question}</InputLabel>
+        <TextField
+          type="number"
+          error={error}
+          onChange={(e) => onChange && onChange(question, e.target.value)}
+          fullWidth
+          size="small"
+        />
+      </Grid>
+    );
+  return (
+    <Grid item {...grid}>
+      <InputLabel style={{ marginBottom: '10px', marginTop: '10px' }}>{question}</InputLabel>
+      <TextField
+        error={error}
+        onChange={(e) => onChange && onChange(question, e.target.value)}
+        fullWidth
+        size="small"
+      />
+    </Grid>
+  );
+};
+
+{
+  /* 
+  <Typography variant="h5" gutterBottom fontWeight={600}>
+Эмнэлгийн мэдээлэл
+</Typography>
+<Grid container width="40vw">
+<Grid item xs={12}>
+    <InputLabel>Эмнэлгийн нэр</InputLabel>
+    <TextField fullWidth size="small"></TextField>
+    <Stack direction="row" justifyContent="space-between">
+    <Grid item xs={5.8}>
+        <InputLabel>Үйл ажиллагааны чиглэл</InputLabel>
+        <TextField
+        fullWidth
+        select
+        value={service}
+        onChange={handleChange}
+        size="small"
+        >
+        {data.services.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+            {option.label}
+            </MenuItem>
+        ))}
+        </TextField>
+    </Grid>
+    <Grid item xs={5.8}>
+        <InputLabel>Үйл ажиллагаа явуулж эхэлсэн огноо</InputLabel>
+        <DesktopDatePicker
+            label={"Date desktop"}
+            inputFormat={"MM/dd/yyyy"}
+            value={value}
+            onChange={(newValue: Date | null) => {
+            // setValue(newValue);
+            }}
+            renderInput={(params: any) => <TextField {...params} />}
+        />
+    </Grid>
+    </Stack>
+    <InputLabel>Холбоо барих утас</InputLabel>
+    <TextField fullWidth size="small"></TextField>
+    <InputLabel>Имэйл хаяг</InputLabel>
+    <TextField fullWidth size="small"></TextField>
+    
+    <InputLabel>Эмнэлгийн вэб сайт</InputLabel>
+    <TextField fullWidth size="small"></TextField>
+</Grid>
+</Grid> */
+}
