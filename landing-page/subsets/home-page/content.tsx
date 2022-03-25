8e1c70c26dacdc153f8f796bca0365f54fb5f5cb
amr2vsn@gmail.com <amr2vsn@Amartuvshins-MacBook-Pro.local>
2022-03-25 18:44:13 +0800

    fix [landing-page] fixed submit

--- landing-page/subsets/home-page/content.tsx
+++ landing-page/subsets/home-page/content.tsx
@@ -12,30 +12,31 @@ const validationSchema = yup.object({
   clinicName: yup.string().required('Clinic name is required'),
   email: yup.string().email('Enter a valid email').required('Email is required'),
   contactNumber: yup.string().required('contact number is required '),
-  city: yup.string().required('Please enter a city name') ,
+  city: yup.string().required('Please enter a city name'),
   district: yup.string().required('Please enter a district name'),
   sub_district: yup.string().required('Please enter a sub district'),
   full_address: yup.string().required('Please enter a full address'),
-  password: yup.string().min(8, 'Password should be of minimum 8 characters length').required('Password is required'),
 });
+// password: yup.string().min(8, 'Password should be of minimum 8 characters length').required('Password is required'),
 
 export const HomePageContent = () => {
   const [formData, setFormData] = useState<any>({});
   const [AddClinic] = useMutation(ADDCLINIC);
   const [snackBar, setSnackBar] = useState<SnackBatTypeParam>({ open: false, message: '', severity: 'info' });
+  // password: 'foobar',
   const formik = useFormik({
     initialValues: {
-      clinicName:'',
+      clinicName: '',
       email: '',
-      password: 'foobar',
       contactNumber: '',
       city: '',
       district: '',
       sub_district: '',
-      full_address:''
+      full_address: ''
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
+      console.log('sss')
       alert(JSON.stringify(values, null, 2));
       // setSnackBar({ message: 'Хүсэлт илгээж байна.', open: true, severity: 'info' });
       // try {
@@ -65,94 +66,96 @@ export const HomePageContent = () => {
 
   return (
     <Box width="100vw" display="flex" flexDirection="column" alignItems="center" marginY={5}>
-      <Grid container justifyContent="center" columns={{ xs: 11 }} rowSpacing={5}>
-        <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-          <QuestionSection
-            label="Эмнэлгийн мэдээлэл"
-            questions={[
-              {
-                id: 'clinicName',
-                question: 'Эмнэлгийн нэр',
-                type: 'input',
-                grid: { xs: 12 },
-                error: Boolean(formik.errors.clinicName),
-                value: formik.values.clinicName,
-                onChange: formik.handleChange,
-              },
-              {
-                id: 'contactNumber',
-                question: 'Холбоо барих утас',
-                type: 'input',
-                grid: { xs: 12 },
-                error: Boolean(formik.errors.contactNumber),
-                value: formik.values.contactNumber,
-                onChange: formik.handleChange,
-              },
-              {
-                id: 'email',
-                question: 'Имэйл хаяг',
-                type: 'input',
-                grid: { xs: 12 },
-                error: Boolean(formik.errors.email),
-                value: formik.values.email,
-                onChange: formik.handleChange,
-              },
-            ]}
-          />
-        </Grid>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container justifyContent="center" columns={{ xs: 11 }} rowSpacing={5}>
+          <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+            <QuestionSection
+              label="Эмнэлгийн мэдээлэл"
+              questions={[
+                {
+                  id: 'clinicName',
+                  question: 'Эмнэлгийн нэр',
+                  type: 'input',
+                  grid: { xs: 12 },
+                  error: Boolean(formik.errors.clinicName),
+                  value: formik.values.clinicName,
+                  onChange: formik.handleChange,
+                },
+                {
+                  id: 'contactNumber',
+                  question: 'Холбоо барих утас',
+                  type: 'input',
+                  grid: { xs: 12 },
+                  error: Boolean(formik.errors.contactNumber),
+                  value: formik.values.contactNumber,
+                  onChange: formik.handleChange,
+                },
+                {
+                  id: 'email',
+                  question: 'Имэйл хаяг',
+                  type: 'input',
+                  grid: { xs: 12 },
+                  error: Boolean(formik.errors.email),
+                  value: formik.values.email,
+                  onChange: formik.handleChange,
+                },
+              ]}
+            />
+          </Grid>
 
-        <Grid item xs={6}>
-          <QuestionSection
-            label="Албан ёсны хаяг"
-            questions={[
-              {
-                id: 'city',
-                question: 'Аймаг / хот',
-                type: 'input',
-                grid: { xs: 5.4 },
-                error: Boolean(formik.errors.city),
-                value: formik.values.city,
-                onChange: formik.handleChange,
-              },
-              {
-                id: 'district',
-                question: 'Дүүрэг',
-                type: 'input',
-                grid: { xs: 5.4 },
-                error: Boolean(formik.errors.district),
-                value: formik.values.district,
-                onChange: formik.handleChange,
-              },
-              {
-                id: 'sub_district',
-                question: 'Хороо',
-                type: 'input',
-                grid: { xs: 5.4 },
-                error: Boolean(formik.errors.sub_district),
-                value: formik.values.sub_district,
-                onChange: formik.handleChange,
-              },
-              {
-                id: 'full_address',
-                question: 'Дэлгэрэнгүй хаяг',
-                type: 'input',
-                grid: { xs: 12 },
-                error: Boolean(formik.errors.full_address),
-                value: formik.values.full_address,
-                onChange: formik.handleChange,
-              },
-            ]}
-          />
-        </Grid>
+          <Grid item xs={6}>
+            <QuestionSection
+              label="Албан ёсны хаяг"
+              questions={[
+                {
+                  id: 'city',
+                  question: 'Аймаг / хот',
+                  type: 'input',
+                  grid: { xs: 5.4 },
+                  error: Boolean(formik.errors.city),
+                  value: formik.values.city,
+                  onChange: formik.handleChange,
+                },
+                {
+                  id: 'district',
+                  question: 'Дүүрэг',
+                  type: 'input',
+                  grid: { xs: 5.4 },
+                  error: Boolean(formik.errors.district),
+                  value: formik.values.district,
+                  onChange: formik.handleChange,
+                },
+                {
+                  id: 'sub_district',
+                  question: 'Хороо',
+                  type: 'input',
+                  grid: { xs: 5.4 },
+                  error: Boolean(formik.errors.sub_district),
+                  value: formik.values.sub_district,
+                  onChange: formik.handleChange,
+                },
+                {
+                  id: 'full_address',
+                  question: 'Дэлгэрэнгүй хаяг',
+                  type: 'input',
+                  grid: { xs: 12 },
+                  error: Boolean(formik.errors.full_address),
+                  value: formik.values.full_address,
+                  onChange: formik.handleChange,
+                },
+              ]}
+            />
+          </Grid>
 
-        <Grid item container xs={6} justifyContent="flex-end">
-          <Grid item xs={4}>
-            <Button onClick={() => formik.handleSubmit} fullWidth variant="contained">
-              Илгээх
-            </Button>
+          <Grid item container xs={6} justifyContent="flex-end">
+            <Grid item xs={4}>
+              <Button fullWidth variant="contained" type="submit">
+                Илгээх
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </form>
 
       <SnackBar
         open={snackBar.open}
