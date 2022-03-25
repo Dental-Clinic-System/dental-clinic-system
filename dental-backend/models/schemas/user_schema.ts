335bbef9599b9f0e555a6b1f30fb034d74e806a6
radnaasuren <radnaasuren@radnaasurens-MacBook-Pro.local>
2022-03-25 19:02:27 +0800

    removed nodemailer

--- dental-backend/models/schemas/user_schema.ts
+++ dental-backend/models/schemas/user_schema.ts
@@ -16,35 +16,5 @@ const UserSchema = new Schema({
     clinicId: String,
     serviceId: String
 });
-UserSchema.post('update', function (doc) {
-    console.log(doc);
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.email,
-            pass: process.env.password
-        }
-    });
-
-    const mailOptions = {
-        from: "dental-clinic from",
-        to: doc.email,
-        subject: 'your request is accepted from superadmin ✔',
-        html: "<b>here</b>",
-    };
-    let data = doc
-    .model("User")
-    .finOneAndUpdate({ _id: doc._id }, { role: "user" });
-    console.log(data)
-
-    transporter.sendMail(mailOptions, function (error: any, info: any) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-});
-
 
 export const UserModel = mongoose.model("User", UserSchema);
