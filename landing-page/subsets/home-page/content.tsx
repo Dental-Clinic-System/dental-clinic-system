a698b684e4d95fe78d6d92c7557a6ad5ace650dc
Zolboo Oktybri <zolboooktybri@Zolboos-MacBook-Pro.local>
2022-03-25 17:29:20 +0800

    Added formik & yup to validate

--- landing-page/subsets/home-page/content.tsx
+++ landing-page/subsets/home-page/content.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import { Box, Grid, Button, TextField, FormHelperText } from '@mui/material';
+import { Box, Grid, Button } from '@mui/material';
 import { useState } from 'react';
 import { QuestionSection } from './question-section';
 import { ADDCLINIC } from '../../graphql';
 import { useMutation } from '@apollo/client';
 import { SnackBar, SnackBatTypeParam } from '../../components/';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
+const validationSchema = yup.object({
+  email: yup.string().email('Enter a valid email').required('Email is required'),
+  password: yup.string().min(8, 'Password should be of minimum 8 characters length').required('Password is required'),
+});
 
 export const HomePageContent = () => {
-
   const [formData, setFormData] = useState<any>({});
   const [AddClinic] = useMutation(ADDCLINIC);
   const [snackBar, setSnackBar] = useState<SnackBatTypeParam>({ open: false, message: '', severity: 'info' });
+  const formik = useFormik({
+    initialValues: {
+      email: 'foobar@example.com',
+      password: 'foobar',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      // setSnackBar({ message: 'Хүсэлт илгээж байна.', open: true, severity: 'info' });
+      // try {
+      //   await AddClinic({ variables: formData });
+
+      //   setSnackBar({ message: 'Таны хүсэлтийг хүлээж авлаа.', open: true, severity: 'success' });
+      // } catch (error) {
+      //   setSnackBar({ message: 'Та мэдээллээ бүтэн оруулна уу!!', open: true, severity: 'error' });
+      // }
+    },
+  });
 
   const onChange = (id: string, _: string, change: string) => {
     setFormData({ ...formData, [id]: change });
   };
 
   const onSubmit = async () => {
-
     setSnackBar({ message: 'Хүсэлт илгээж байна.', open: true, severity: 'info' });
     try {
       await AddClinic({ variables: formData });
@@ -29,16 +51,11 @@ export const HomePageContent = () => {
     }
   };
 
-  
-
-  
   return (
     <Box width="100vw" display="flex" flexDirection="column" alignItems="center" marginY={5}>
       <Grid container justifyContent="center" columns={{ xs: 11 }} rowSpacing={5}>
-
         <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
           <QuestionSection
-            onChange={onChange}
             label="Эмнэлгийн мэдээлэл"
             questions={[
               {
@@ -47,24 +64,33 @@ export const HomePageContent = () => {
                 type: 'input',
                 grid: { xs: 12 },
                 error: !formData['clinicName'],
+                value: 'wtf',
+                onChange: (str: string) => console.log(str),
               },
-            
               {
                 id: 'contactNumber',
                 question: 'Холбоо барих утас',
                 type: 'input',
                 grid: { xs: 12 },
                 error: !formData['contactNumber'],
+                value: '',
+                onChange: (str: string) => console.log(str),
               },
-              { id: 'email', question: 'Имэйл хаяг', type: 'input', grid: { xs: 12 }, error: !formData['email'] }
-              
+              {
+                id: 'email',
+                question: 'Имэйл хаяг',
+                type: 'input',
+                grid: { xs: 12 },
+                error: Boolean(formik.errors.email),
+                value: formik.values.email,
+                onChange: formik.handleChange,
+              },
             ]}
           />
         </Grid>
 
         <Grid item xs={6}>
           <QuestionSection
-            onChange={onChange}
             label="Албан ёсны хаяг"
             questions={[
               {
@@ -73,14 +99,26 @@ export const HomePageContent = () => {
                 type: 'input',
                 grid: { xs: 5.4 },
                 error: !formData['city'],
+                value: '',
+                onChange: (str: string) => console.log(str),
               },
-              { id: 'district', question: 'Дүүрэг', type: 'input', grid: { xs: 5.4 }, error: !formData['district'] },
+              {
+                id: 'district',
+                question: 'Дүүрэг',
+                type: 'input',
+                grid: { xs: 5.4 },
+                error: !formData['district'],
+                value: '',
+                onChange: (str: string) => console.log(str),
+              },
               {
                 id: 'sub_district',
                 question: 'Хороо',
                 type: 'input',
                 grid: { xs: 5.4 },
                 error: !formData['sub_district'],
+                value: '',
+                onChange: (str: string) => console.log(str),
               },
               {
                 id: 'full_address',
@@ -88,6 +126,8 @@ export const HomePageContent = () => {
                 type: 'input',
                 grid: { xs: 12 },
                 error: !formData['full_address'],
+                value: '',
+                onChange: (str: string) => console.log(str),
               },
             ]}
           />
@@ -95,24 +135,20 @@ export const HomePageContent = () => {
 
         <Grid item container xs={6} justifyContent="flex-end">
           <Grid item xs={4}>
-            <Button onClick={onSubmit} fullWidth variant="contained">
+            <Button onClick={() => formik.handleSubmit} fullWidth variant="contained">
               Илгээх
             </Button>
           </Grid>
         </Grid>
-      
       </Grid>
-      
 
       <SnackBar
         open={snackBar.open}
         handleClose={() => setSnackBar({ ...snackBar, open: false })}
         message={snackBar.message}
-        severity={snackBar.severity}   
+        severity={snackBar.severity}
       />
-
     </Box>
   );
 };
 // };
-
