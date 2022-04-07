fb58117930036e6161034cd029339295016caaa1
IceCream <ermuunee10@gmail.com>
2022-04-07 16:42:41 +0800

    Feature - [front-end] add data grid on service

---
+++ front/src/components/common/table.tsx
@@ -0,0 +1,16 @@
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+
+export const Table = () => {
+  return (
+    <div>
+      {/* <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      /> */}
+    </div>
+  );
+};
