fd00f89962c875a00a8a4c2b06a69b07dc33f3c2
Anand-Ochir <anadaochir@gmail.com>
2022-04-08 20:13:57 +0800

    profile page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import routes from '../router';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import ThemeProvider from './theme/ThemeProvider';
+import { CssBaseline } from '@mui/material';
 
 const App = () => {
-  return (
-      <div className="App">
-        happy hacking!!!
-      </div>
-  );
+  const content = useRoutes(routes);
+  return(
+    <ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <CssBaseline />
+        {content}
+      </LocalizationProvider>
+    </ThemeProvider>
+  )
 }
 
 export default App;
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,1 +1,2 @@
-export {}
+export * from './profile'
+export * from './users'
