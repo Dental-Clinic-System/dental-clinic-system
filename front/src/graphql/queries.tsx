030cf267325d485ce89cc43d77b46a57a48c2243
Ermuun-Orgil <55132472+Ermuun-Orgil@users.noreply.github.com>
2022-04-11 21:17:23 +0800

    163 service datagrid (#170)
    * Feature - [front-end] add data grid on service
    
    * clear
    
    * Feature - [front-end] add service query
    
    * delete table
    
    Co-authored-by: Enkhbayar G <enkhbayar.gan@gmail.com>

--- front/src/graphql/queries.tsx
+++ front/src/graphql/queries.tsx
@@ -1,29 +1,44 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_PATIENTS = gql`
- query GetPatients {
-  getPatients {
-    _id
-    lastname
-    firstname
-    age
-    birth
-    registration_number
-    city
-    sex
-    district
-    address_description
-    committee
-    role
-    phone
-    home_phone
-    email
-    doctor
-    card_number
-    sysdate
+  query GetPatients {
+    getPatients {
+      _id
+      lastname
+      firstname
+      age
+      birth
+      registration_number
+      city
+      sex
+      district
+      address_description
+      committee
+      role
+      phone
+      home_phone
+      email
+      doctor
+      card_number
+      sysdate
+    }
   }
-}   
-`
+`;
+
+export const GET_SERVICES = gql`
+  query GetServices {
+    getServices {
+      _id
+      classification
+      service_name
+      code
+      short
+      description
+      price
+    }
+  }
+`;
+
 export const GetClinics = gql`
 query GetClinics {
     getClinics {
@@ -51,4 +66,4 @@ query GetClinics {
       status
     }
   }
-`
+`;
