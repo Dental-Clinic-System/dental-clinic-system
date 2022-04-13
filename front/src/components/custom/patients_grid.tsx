662a4959719e47ebe0c9878b9613704285d7b62a
bilguun1324 <bilguun1324@gmail.com>
2022-04-13 17:41:48 +0800

    create common modals

--- front/src/components/custom/patients_grid.tsx
+++ front/src/components/custom/patients_grid.tsx
@@ -4,52 +4,6 @@ import { Loading } from "..";
 import { Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
-const columns = [
-  { field: "_id", width: 150 },
-  {
-    field: "firstname",
-    headerName: "Өвчтөний нэр",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "lastname",
-    headerName: "Өвчтөний Овог",
-    width: 150,
-    editable: true,
-  },
-  { field: "age", headerName: "Нас", width: 70, editable: true },
-  { field: "birth", headerName: "Төрсөн өдөр", width: 150, editable: true },
-  {
-    field: "registration_number",
-    headerName: "Регистийн дугаар",
-    width: 150,
-    editable: true,
-  },
-  { field: "sex", headerName: "Хүйс", width: 70, editable: true },
-  {
-    field: "address_description",
-    headerName: "Хаяг",
-    width: 150,
-    editable: true,
-  },
-  { field: "phone", headerName: "Утасны дугаар", width: 150, editable: true },
-  {
-    field: "home_phone",
-    headerName: "Гэрийн утасны дугаар",
-    width: 150,
-    editable: true,
-  },
-  { field: "email", headerName: "Имэйл хаяг", width: 150, editable: true },
-  { field: "doctor", headerName: "Эмчлэгч эмч", width: 150, editable: true },
-  {
-    field: "card_number",
-    headerName: "Картын дугаар",
-    width: 150,
-    editable: true,
-  },
-];
-
 type PatientsGridType = {
   setSelectedPatient: Function;
 };
@@ -59,21 +13,37 @@ export const PatientsGrid: React.FC<PatientsGridType> = ({
 }) => {
   const { loading, data } = useQuery(GET_PATIENTS);
 
-  return (
-    <Box>
-      {loading && <Loading />}
-      <div style={{ height: 400, width: "100%" }}>
-        <DataGrid
-          getRowId={(row) => row._id}
-          rows={data ? data.getPatients : []}
-          columns={columns}
-          checkboxSelection
-          disableSelectionOnClick
-          onSelectionModelChange={(item: any) => {
-            setSelectedPatient(item);
-          }}
-        />
-      </div>
-    </Box>
-  );
-};
+    return (
+        <Box>
+            {loading && <Loading/>}
+            <Box sx={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    getRowId={(row) => row._id}
+                    rows={data ? data.getPatients : []}
+                    columns={columns}
+                    checkboxSelection
+                    disableSelectionOnClick 
+                    onSelectionModelChange={(item: any) => {
+                        setSelectedPatient(item)
+                    }}
+                />
+            </Box>
+        </Box>
+    );
+}
+
+const columns = [
+    { field: '_id', width: 150 },
+    { field: 'firstname', headerName: 'Өвчтөний нэр', width: 150, editable: true },
+    { field: 'lastname', headerName: 'Өвчтөний Овог', width: 150, editable: true },
+    { field: 'age', headerName: 'Нас', width: 70, editable: true },
+    { field: 'birth', headerName: 'Төрсөн өдөр', width: 150, editable: true },
+    { field: 'registration_number', headerName: 'Регистийн дугаар', width: 150, editable: true },
+    { field: 'sex', headerName: 'Хүйс', width: 70, editable: true },
+    { field: 'address_description', headerName: 'Хаяг', width: 150, editable: true },
+    { field: 'phone', headerName: 'Утасны дугаар', width: 150, editable: true },
+    { field: 'home_phone', headerName: 'Гэрийн утасны дугаар', width: 150, editable: true },
+    { field: 'email', headerName: 'Имэйл хаяг', width: 150, editable: true },
+    { field: 'doctor', headerName: 'Эмчлэгч эмч', width: 150, editable: true },
+    { field: 'card_number', headerName: 'Картын дугаар', width: 150, editable: true },
+];
