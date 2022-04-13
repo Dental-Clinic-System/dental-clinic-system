a7c9b400f6befda3aaaede4f99528731bc516f86
Enko Ganbaatar <enkhbayar.gan@gmail.com>
2022-04-13 16:39:08 +0800

    example code

---
+++ front/src/interfaces/IPatient.ts
@@ -0,0 +1,19 @@
+export interface IPatient {
+  lastname: String;
+  firstname: String;
+  birth: String;
+  age: String;
+  registration_number: String;
+  sex: String;
+  city: String;
+  district: String;
+  committee: String;
+  address_description: String;
+  role: String;
+  phone: String;
+  home_phone: String;
+  email: String;
+  doctor: String;
+  card_number: String;
+  sysdate: String;
+}
