aedf6c1ae37abdae6471f4e8d9708edf29a9d199
bilguun1324 <bilguun1324@gmail.com>
2022-04-18 16:13:49 +0800

    updated patient structure in back end

--- dental-backend/resolvers/query.ts
+++ dental-backend/resolvers/query.ts
@@ -1,4 +1,4 @@
-import { ClinicModel, UserModel, PatientModel, ServiceModel, AppointmentModel, PatientHistoryModel, PermissionModel, RoleModel, ProviceModel, DistrictModel, CommitteeModel } from '../models/schemas'
+import { ClinicModel, UserModel, PatientModel, ServiceModel, AppointmentModel, PermissionModel, RoleModel, ProviceModel, DistrictModel, CommitteeModel } from '../models/schemas'
 
 const queries = {
     getUsers: async (_: any, params: any) => await UserModel.find(params),
@@ -6,7 +6,6 @@ const queries = {
     getPatients: async (_: any, params: any) => await PatientModel.find(params),
     getServices: async (_: any, params: any) => await ServiceModel.find(params),
     getAppointments: async (_: any, params: any) => await AppointmentModel.find(params),
-    getPatientHistories: async (_: any, params: any) => await PatientHistoryModel.find(params),
     getPermissions: async (_: any, params: any) => await PermissionModel.find(params),
     getRoles: async (_: any, params: any) => await RoleModel.find(params),
     getProvinces: async (_: any, params: any) => await ProviceModel.find(params),
