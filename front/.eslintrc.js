e2c33bda5f6e07e7beb907b9a7c3a951e1586db6
Zolboo Oktybri <zolboooktybri@Zolboos-MacBook-Pro.local>
2022-04-22 16:50:50 +0800

    eslint rules removed

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -3,53 +3,9 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb-typescript/base',
-    'plugin:prettier/recommended'
-  ],
   parserOptions: {
     project: ['tsconfig.json'],
     tsconfigRootDir: __dirname
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  rules: {
-    'no-console': 'warn',
-    'no-empty-function': ['error', { allow: ['arrowFunctions'] }],
-    '@typescript-eslint/no-empty-function': [
-      'error',
-      { allow: ['arrowFunctions'] }
-    ],
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-loss-of-precision': 'off',
-    'max-lines': [
-      'error',
-      { max: 1000, skipBlankLines: true, skipComments: true }
-    ],
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        selector: 'variable',
-        format: ['camelCase', 'PascalCase', 'UPPER_CASE']
-      }
-    ],
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-misused-promises': [
-      'error',
-      {
-        checksVoidReturn: false
-      }
-    ],
-    'import/no-unresolved': 'off',
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function'
-      }
-    ]
-  }
+  plugins: ['react', '@typescript-eslint', 'prettier']
 };
