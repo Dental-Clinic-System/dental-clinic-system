301a54a58f1e7cff7887beb4b9ac50b8f9f8ce6b
khash531 <n.ligfe@gmail.com>
2022-04-24 18:01:40 +0800

    Side bar style changed

--- front/src/layout/SideBar.tsx
+++ front/src/layout/SideBar.tsx
@@ -1,35 +1,45 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Divider,
-  Toolbar,
-  ListItem,
-  List,
-  ListItemText,
-} from "@mui/material";
+import React from 'react';
+import { Box, Divider, Toolbar, ListItem, List, ListItemText } from '@mui/material'
+import { makeStyles } from '@mui/styles';
 import { Link, useLocation } from "react-router-dom";
 
+const useStyles = makeStyles({
+  root: {
+    color: '#404040',
+  },
+  active: {
+    color: '#467db5',
+    background: '#e8f6ff',
+    borderLeft: `5px solid #467db5`,
+  },
+});
+
 export const Sidebar = () => {
-  const [selectedIndex, setSelectedIndex] = useState();
+  const classes = useStyles();
+  const { pathname } = useLocation();
 
   const route = [
     { path: "/", text: "Home" },
-    { path: "appointment", text: "Appointments" },
-    { path: "users", text: "Stafff" },
-    // { path: "clinics", text: "Clinics" },
-    { path: "services", text: "Services" },
-    { path: "patients", text: "Patients" },
-    { path: "patient-histories", text: "Patient Histories" },
+    { path: "/appointment", text: "Appointments" },
+    { path: "/users", text: "Users" },
+    { path: "/clinics", text: "Clinics" },
+    { path: "/services", text: "Services" },
+    { path: "/patients", text: "Patients" },
   ];
 
   return (
     <Box>
       <Toolbar />
-      {sessionStorage.getItem("clinicTitle")}
       <Divider />
       <List>
         {route.map((data) => (
-          <ListItem button component={Link} to={data.path}>
+          <ListItem
+            to={data.path}
+            key={data.path} 
+            component={Link}
+            selected={data.path === pathname}
+            className={data.path === pathname ? classes.active : classes.root}
+          >
             <ListItemText primary={data.text} />
           </ListItem>
         ))}
