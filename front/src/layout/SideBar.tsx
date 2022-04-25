2390aa940c23bcfb6e673d57499febd67d36e0b7
khash531 <n.ligfe@gmail.com>
2022-04-25 16:05:10 +0800

    undid accidental change

--- front/src/layout/SideBar.tsx
+++ front/src/layout/SideBar.tsx
@@ -30,6 +30,7 @@ export const Sidebar = () => {
   return (
     <Box>
       <Toolbar />
+      {sessionStorage.getItem("clinicTitle")}
       <Divider />
       <List>
         {route.map((data) => (
